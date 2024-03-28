32f16067e8a16f5522a66261a95b7771d42c175e
Likhitha N <138875891+9ilu@users.noreply.github.com>
2024-03-28 11:25:13 +0530

    last commit

--- server.js
+++ server.js
@@ -1,84 +1,44 @@
-const http = require("http");
+const express = require("express"); //to get express
 
-const port = 8085;
+const app = express(); //initialize express
 
-// http methods
-// >> GET:Inorder to get data from the server
-// >>POST:Sending data to server
-// >>DELETE:Deleting data from database
-// >>PATCH:updating certain fields
-// >>PUT:full update
+app.use(express.json()); //use express
 
-const todolist = ["learn", "apply things", "succed"];
+const port = 8081;
 
+const toDoList = ["learn", "apply things", "succed"];
 
-http
-    .createServer((req, res) => {
-        const { method, url } = req;
-        // 
-        if (url === "/todos") {
-            if (method === "GET") {
-                res.writeHead(200);
-                res.write(todolist.toString());
+app.get('/todos', (req, res) => {
+    res.status(200).send(toDoList);
+});
 
-            } else if (method === "POST") {
-                let body = "";
-                req
-                    .on('error', (err) => {
-                        console.log(err);
-                    }).on('data', (chunk) => {
-                        body += chunk;
-                        console.log(chunk);
-                    }).on('end', () => {
-                        body = JSON.parse(body);
+app.post('/todos', (req, res) => {
+    let newToDoItem = req.body.name;
+    toDoList.push(newToDoItem);
+    res.status(201).send({ message: "Task added successfully" });
+});
 
-                        let newToDo = todolist;
-                        newToDo.push(body.item);
-                        console.log(newToDo);
-                        // console.log("data:", body);
-                    });
-            } else if (method === "DELETE") {
-                let body = "";
-                req.on("error", (err) => {
-                        console.error(err);
-                    }).on("data", (chunk) => {
-                        body += chunk;
-                    })
-                    .on("end", () => {
-                        body = JSON.parse(body);
-                        let deleteThisItem = body.item;
-                        for (let i = 0; i < todolist.length; i++) {
-                            if (todolist[i] === deleteThisItem) {
-                                todolist.splice(i, 1);
-                                break;
-                            } else {
-                                console.error("Error:Match not found!");
-                                break;;
-                            }
-                        }
-                        // todolist.find((elem, index) => {
-                        //     if (elem === deleteThisItem) {
-                        //         todolist.splice(index, 1);
-                        //     } else {
-                        //         console.error("Error: Match Not Found");
-                        //         console.exit();
-                        //     }
-                        // });
-                    });
-            } else {
-                res.writeHead(501);
-            }
-
-        } else {
-            res.writeHead(404);
+app.delete('*', (req, res) => {
+    const deleteThisItem = req.body.name;
+    toDoList.find((elem, index) => {
+        if (elem === deleteThisItem) {
+            toDoList.splice(index, 1);
         }
-        res.end();
-        //     res.writeHead(200, { "Content-Type": "test/html" });
-        //     res.write("<h2> Hey server started : </h2>");
-        //     res.end();
-    })
-    .listen(port, () => {
-        console.log(`NodeJs Server started Running on Port ${port}`);
+
     });
-// css tricks.com
-//javascript.info
+    res.status(202).send({ message: `Deleted item  ${req.body.name}` });
+});
+
+app.all("/todo", (req, res) => {
+    res.status(501).send();
+});
+
+app.listen(port, () => {
+    console.log(`NoseJs server Running on Port ${port}`);
+});
+
+
+
+// app.post('/todos', (req, res) => {});
+
+// app.delete('/todos', (req, res) => {});
